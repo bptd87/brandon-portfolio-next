88e2880f36161fc7fc76cb47a7191a3c34ad347c
Brandon PT Davis <Brandon@brandonptdavis.com>
2026-03-26 10:54:44 -0700

    Remove legacy SPA and table-driven admin stack

--- components/legacy/Providers.tsx
+++ components/legacy/Providers.tsx
@@ -1,38 +1,17 @@
 "use client";
 
-import { useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HelmetProvider } from "react-helmet-async";
 import { ThemeProvider } from "next-themes";
 
 import { TooltipProvider } from "../../client/src/components/ui/tooltip";
 import { Toaster } from "../../client/src/components/ui/sonner";
 
 export function LegacyProviders({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 1000 * 60 * 5,
-            gcTime: 1000 * 60 * 10,
-            refetchOnWindowFocus: false,
-            retry: 1,
-          },
-        },
-      })
-  );
-
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} forcedTheme="dark">
-      <HelmetProvider>
-        <QueryClientProvider client={queryClient}>
-          <TooltipProvider>
-            {children}
-            <Toaster />
-          </TooltipProvider>
-        </QueryClientProvider>
-      </HelmetProvider>
+      <TooltipProvider>
+        {children}
+        <Toaster />
+      </TooltipProvider>
     </ThemeProvider>
   );
 }
--- server/routers/adminProcedure.ts
+++
@@ -1,9 +0,0 @@
-import { TRPCError } from "@trpc/server";
-import { protectedProcedure } from "../_core/trpc";
-
-export const adminProcedure = protectedProcedure.use(({ ctx, next }) => {
-  if (ctx.user.role !== "admin") {
-    throw new TRPCError({ code: "FORBIDDEN", message: "Admin access required" });
-  }
-  return next({ ctx });
-});
